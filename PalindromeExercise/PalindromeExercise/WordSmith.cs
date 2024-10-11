a6619dc8e853a63e5d14e7109a7a0dbd8ae0665c
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-10-11 11:40:56 -0700

    completed palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string word)
+    {
+        word = word.ToLower();
+
+        int start = 0;
+        int end = word.Length - 1;
+
+        while (start < end)
+        {
+            if (word[start] != word[end])
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("madam", true)]
+        public void PalindromeTest(string word, bool expected)
         {
+            var test = new WordSmith();
+            
+            bool actual = test.IsAPalindrome(word);
 
+            Assert.Equal(expected, actual);
         }
     }
 }
